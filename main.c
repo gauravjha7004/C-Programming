d7be161fedd361bfea936d63a11e235d84621f7e
gauravjha7004 <132380712+gauravjha7004@users.noreply.github.com>
2023-05-02 09:30:14 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	char name[20];
+	char college[50];
+	char branch[30];
+
+
+	printf("enter your name");
+	scanf("%s",&name);
+	printf("enter your college name ");
+	scanf("%s",&college);
+	printf("enter your branch");
+	scanf("%s",&branch);
+	printf("\n my name is :- %s", name);
+	printf("\n my college name is:- %s",college);
+	printf("\n my branch is :- %s",branch);
+
+	
+	
+	
+	return 0;
+}

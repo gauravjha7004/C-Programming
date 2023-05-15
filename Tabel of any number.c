aa4891c858b78d51e0ee5f9f54c5781ab2265769
gauravjha7004 <132380712+gauravjha7004@users.noreply.github.com>
2023-05-15 08:20:47 -0700

    Add files via upload

---
+++ Tabel of any number.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+
+int num ,i=1;
+printf("enter any number");
+scanf("%d",&num);
+
+for(i;i<=10;i++)
+{
+	printf("%d\n",i*num);
+}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
